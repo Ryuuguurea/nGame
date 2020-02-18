662a3889cd5ec421a4635b598cbb462c330fbb4e
ryuuguurea <ryuuguurea@163.com>
2020-02-18 23:08:08 +0800

    调试配置

--- nzRpg/Assets/scripts/main.cs
+++ nzRpg/Assets/scripts/main.cs
@@ -7,7 +7,7 @@ public class main : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("hello unity");
     }
 
     // Update is called once per frame
